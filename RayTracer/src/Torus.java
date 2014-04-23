7d994f8af181c3db0953563474353b86f19e1d64
sjf5gf <sjf5gf@virginia.edu>
2014-04-23 18:23:52 -0400

    began torus...

---
+++ RayTracer/src/Torus.java
@@ -0,0 +1,43 @@
+
+public class Torus extends Obstacle{
+	private Vector center;
+	private Color color;
+	private double majorRadius;
+	private double minorRadius;
+	@Override
+	public double findIntersection(Ray ray) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Vector getNormal(Vector location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor(Vector pt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector getVector() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getRadius() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
